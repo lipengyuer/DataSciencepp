f0b9d7ca7ad356ed04c93c183ff0f4c86abde2c4
lepengyuer <lipengyuer@126.com>
2019-09-25 22:20:04 +0800

    添加

---
+++ WordCount.cpp
@@ -0,0 +1,14 @@
+//统计语料中的字的频数
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    string data;
+    cout << "asdasd" <<endl;
+    ifstream fheadle;
+    fheadle.open("C:/Users/Administrator/Desktop/想法.txt");
+    fheadle >> data;
+    cout << data <<endl;
+
+}
